124371bffe3667b78e2644b5a9f18f984c6151b3
mgilch <michael-gilch@t-online.de>
2023-07-21 01:14:58 +0200

    hello stock market

--- src/stock/Main.java
+++ src/stock/Main.java
@@ -3,6 +3,6 @@ package stock;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("hello stock market");
     }
 }
