70eadfea420f8a93dc380815253d43f48f8bc770
mgilch <michael-gilch@t-online.de>
2023-07-27 14:52:31 +0200

    JavaFX works now + Hello Stock Market

--- src/main/java/com/stock/Launcher.java
+++ src/main/java/com/stock/Launcher.java
@@ -1,10 +1,9 @@
 package com.stock;
 
-
 import com.stock.ui.Window;
 import javafx.application.Application;
-public class Launcher {
 
+public class Launcher {
     public static void main(String[] args) {
         Application.launch(Window.class, args);
     }
--- src/main/java/com/stock/StockWatchList.java
+++ src/main/java/com/stock/StockWatchList.java
@@ -4,10 +4,10 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 
 public class StockWatchList {
-    public StockWatchList(Canvas canvas, Scene rootScene){
-
+    public StockWatchList(Canvas canvas, Scene rootScene) {
     }
+
     public void run() {
-        System.out.println("application running");
+        System.out.println("Hello Stock Market");
     }
 }
